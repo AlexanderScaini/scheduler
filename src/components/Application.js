d4f48d6e4471bada171b1deb119880718e792a71
Panini <alexanderscaini@gmail.com>
2020-10-28 16:11:31 -0400

    added axios and a get request to fetch the days from the api on intial render

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
@@ -6,24 +7,6 @@ import Appointment from "components/Appointment";
 import "components/Application.scss";
 
 
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -74,7 +57,14 @@ const appointments = [
 
 export default function Application(props) {
 
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday")
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then(response => {
+      setDays(response.data)
+    })
+  }, [])
 
   return (
     <main className="layout">
