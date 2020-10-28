6a4ca652be6319a89aa0cf799e453e84b4a1617b
Panini <alexanderscaini@gmail.com>
2020-10-27 20:26:53 -0400

    got the application showing the correct information

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 import "components/Application.scss";
 
@@ -22,6 +23,52 @@ const days = [
     name: "Wednesday",
     spots: 0,
   },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time:"2pm",
+    interview: {
+      student: "Alexander Scaini",
+      interviewer: { id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png" 
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Jason Long",
+      interviewer: { 
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg"
+      }
+    }
+  }
 ];
 
 
@@ -54,7 +101,9 @@ export default function Application(props) {
 
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          return <Appointment key={appointment.id} {...appointment} />
+        })}
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,17 @@
 import React from "react";
+
+import Header from 'components/Appointment/Header.js';
+import Empty from 'components/Appointment/Empty.js';
+import Show from 'components/Appointment/Show.js';
+
 import 'components/Appointment/styles.scss'
 
 export default function Appointment(props) {
   return (
-    <></>
+    <div>
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty/>}
+    </div>
+    
   )
 }
