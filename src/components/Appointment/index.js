4024da07a4b2880bd6ddda35bea9d21692a3cef9
Panini <alexanderscaini@gmail.com>
2020-10-29 18:19:42 -0400

    added the transition to the appointment component and the create and cancel buttons work

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,33 @@ import React from "react";
 import Header from 'components/Appointment/Header.js';
 import Empty from 'components/Appointment/Empty.js';
 import Show from 'components/Appointment/Show.js';
+import Form from 'components/Appointment/Form.js';
+
+import { useVisualMode } from '../../hooks/useVisualMode.js'
 
 import 'components/Appointment/styles.scss'
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";  
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+
   return (
     <div>
-      <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty/>}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (<Show 
+        student={props.interview.student} 
+        interviewer={props.interview.interviewer}
+      />)}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </div>
     
   )
 }
+
+
