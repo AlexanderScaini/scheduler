d8d6c57e815964fd76260bf12f37c60b736a81ec
Panini <alexanderscaini@gmail.com>
2020-10-30 13:57:15 -0400

    mixed bug that prevented the show component to display properly

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,7 +47,10 @@ export default function Application(props) {
         axios.get("http://localhost:8001/api/interviewers")
       ),
     ]).then((all) => {
-      console.log(all)
+      // console.log(all)
+      console.log(all[0].data)
+      console.log(all[1].data)
+      console.log(all[2].data)
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
 
